6e98ffbd66acbfcd5f2b28735113d30220f5f788
ilyakrup <ilyakrup1@yandex.ru>
2022-04-01 16:15:30 +0400

    create widow registration

---
+++ test.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="test.css">
+    <title>Document</title>
+</head>
+<body>
+<main class="form-signin">
+    <form action="account.php" method="post" calss="for">
+        <div class="logo">
+            <a href=""><img class="mb-4" src="image/logo.jpg" alt="" width="72" height="57"></a>
+            <h1 class="h3 mb-3 fw-normal">Зарегестрироваться</h1>
+        </div>
+        <div class="form-floating">
+            <input name="mail" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+            <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Пароль</label>
+        </div>
+        <input value="Зарегестрироваться" class="w-100 btn btn-lg btn-primary" type="submit">
+    </form>
+</main>
+</body>
+</html>
