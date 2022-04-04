61cf0ed13a295b2cb1eba6b1ff94412674e8c2ec
ilyakrup <ilyakrup1@yandex.ru>
2022-04-04 21:54:33 +0400

    Редактирование и создание бэка для регистрации

--- auth.php
+++ auth.php
@@ -8,14 +8,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="test.css">
-    <title>Document</title>
-</head>
+    <title>Авторизация</title>
+</head
 <body>
 <main class="form-signin">
-    <form action="backregister.php" method="post" calss="for">
+    <form action="" method="post" calss="for">
         <div class="logo">
-            <a href=""><img class="mb-4" src="image/logo.jpg" alt="" width="72" height="57"></a>
-            <h1 class="h3 mb-3 fw-normal">Зарегестрироваться</h1>
+            <a href="test.php"><img class="mb-4" src="image/logo.jpg" alt="" width="72" height="57"></a>
+            <h1 class="h3 mb-3 fw-normal">Авторизация</h1>
         </div>
         <div class="form-floating">
             <input name="mail" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
@@ -25,7 +25,8 @@
             <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
             <label for="floatingPassword">Пароль</label>
         </div>
-        <input value="Зарегестрироваться" class="w-100 btn btn-lg btn-primary" type="submit">
+        <input value="Войти" class="w-100 btn btn-lg btn-primary" type="submit">
+        <a href="test.php">Зарегестрироваться</a>
     </form>
 </main>
 </body>
